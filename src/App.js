095f4c6331de6ad53715797215266b6245bdbbf6
Maaz Khan <maazkhan7454@gmail.com>
2023-10-14 15:46:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function App() {
+  return(
+    <>
+      <DateCounter />
+    </>
   );
 }
 
-export default App;
+const initialState = {count: 0, step: 1};
+
+function reducer(state, action) {
+  // console.log(state, action);
+  switch(action.type) {
+    case 'inc':
+      return { ...state, count: state.count + state.step }
+
+    case 'dec':
+      return { ...state, count: state.count - state.step }
+    
+    case 'setCount':
+      return { ...state, count: action.payload }
+
+    case 'setStep':
+      return { ...state, step: action.payload }
+
+    case 'reset':
+      return { count: 0, step: 1 }
+    
+    default: 
+      return 'Undefined action performed';
+  }
+}
+
+function DateCounter() {
+  // const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
+
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + count);
+
+  const reset = function () {
+    dispatch({type: 'reset'})
+  };
+
+  return (
+    <div className="counter">
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => dispatch({type: 'setStep', payload: Number(e.target.value)})}
+        />
+        <span>{step}</span>
+      </div>
+
+      <div>
+        <button onClick={() => dispatch({type: 'dec'})}>-</button>
+        <input value={count} onChange={(e) => dispatch({type: 'setCount', payload: Number(e.target.value)})} />
+        <button onClick={() => dispatch({type: 'inc'})}>+</button>
+      </div>
+
+      <p>{date.toDateString()}</p>
+
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+}
